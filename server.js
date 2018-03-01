d2ac1a6ff520194cb3ac4016aff15806994b5d77
fuzzytyrion <matthewrdennis@gmail.com>
2018-03-01 07:20:15 +0000

    add handlebars templates

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send({
-        name: 'Edberg',
-        likes: ['guiter', 'other stuff']
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to City 17'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
